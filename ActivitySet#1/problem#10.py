17882d986b3439ca10cc1f0179f4988f948e47d5
Sruthi <93090631+ChinthimiSruthi@users.noreply.github.com>
2022-05-16 06:41:42 +0000

    pb 10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,12 @@
-# Dictionaries
-
-filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+d={}
+for i in handle:
+    if i.startswith("From "):
+        name=i.split()[1]
+        d[name] = d.get(name,0) + 1
+#print(d)
+user = max(d.items(),key=lambda x:x[1])
+print(user[0],user[1])
